8d7722e0b2dd51296cb5a0955ec1c173ecc4f21c
samcr <samcralph@outlook.com>
2024-03-15 21:31:41 -0400

    Added team descriptions to the game methods

--- TicTacToeDigital.java
+++ TicTacToeDigital.java
@@ -35,24 +35,24 @@ public class TicTacToeDigital {
         int optiontoChosePLayer = 0;
 
         //Title to the game
-        println( "\n***************************")
-        println("*      Tic-Tac-Toe        *")
-        println("***************************")
+        println( "\n***************************");
+        println("*      Tic-Tac-Toe        *");
+        println("***************************");
 
         //Explain the program to the user
-        println("\nThis program provide a fun game that is") 
-        println("created to be enjoyed that is Tic-Tac-Toe,")
-        println("where you vs a player or a AI can play")
-        println("and win but also lose or draw.")
+        println("\nThis program provide a fun game that is"); 
+        println("created to be enjoyed that is Tic-Tac-Toe,");
+        println("where you vs a player or a AI can play");
+        println("and win but also lose or draw.");
 
         //Menu of selections of gamemodes 
-        println("\n-------- Menu -------")
-        println("1. Vs a local player")
-        println("2. Vs an AI machine")
-        println("0. Quit")
+        println("\n-------- Menu -------");
+        println("1. Vs a local player");
+        println("2. Vs an AI machine");
+        println("0. Quit");
 
         //Let the user decide the option
-        println("Please choose an option:")
+        println("Please choose an option:");
         optiontoChosePLayer = in.nextInt();
 
         //leave a space
@@ -64,7 +64,7 @@ public class TicTacToeDigital {
                 //Vs a local player
                 case 1:
 
-                 //Add break
+                //Add break
                  break; 
 
                 //Vs an ai machine
@@ -88,36 +88,41 @@ public class TicTacToeDigital {
 
         //This method will show you the scoreboard of the games won and lose of each player
         public static void Scoreboard(int board) {
+            //Hugo will be responsible for the scoreboard, implemented through some variable exposistion system that updates with every game 
 
         }
 
         //This method will track how the match is going
         public static void Tracking(int track) {
+            //Hugo will create this feature that will track the players' records and feeds them into the various other game components that require them, such as the Scoreboard method 
 
         }
 
         //This method shows replay of the entire round
          public static void replay(int rep) {
+            //By recording the history of the players' movements, Hugo will create a system that reinacts the posistion data per the user's request.
 
         }
 
         //This method adds the player's names to know which one is playing
         public static void playerName(int name) {
+            //Hugo will make a simple playerName method that allows user's to input their desired screename and compete with it throughout the game  
 
         }
 
         //This method is for the AI opponent to go up against
         public static void AI(int machine) {
+            //Sam is responsible for this component, and will most likely be implemented through some random move generator of the sorts or similar system
 
         }
-
+ 
         //This method is used to print out the board
          public static void boardTicTacToe(int board) {
-
+            //Co-operation between Max and Sam which establishes the bulk of the games logic. This method is realistically a representative for the main game logic loop, in which a board is printed with posistion updates according to coordinates supplied through arrays. 
         }
         
        //Shortcut to use print
-       public static void print(String msg) {
+        public static void print(String msg) {
         System.out.print(msg);
         }
 
