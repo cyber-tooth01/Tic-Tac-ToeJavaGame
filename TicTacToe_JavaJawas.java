27d9f4c95f0157c82f6dce4f69a0e0b750c3a9e9
samcr <samcralph@outlook.com>
2024-03-23 16:56:39 -0400

    Co-authored-by: Cordova de Varona Hugo <26-016@sanignacio.pr> Co-authored-by: CSI-Maximiliano-Davila <MaximilianoDavila@users.noreply.github.com>

--- TicTacToe_JavaJawas.java
+++ TicTacToe_JavaJawas.java
@@ -38,6 +38,9 @@ public class TicTacToe_JavaJawas {
     public static char currentPlayerMark;
     public static boolean gameEnded;
 
+    public static int scoreX = 0;
+    public static int scoreO = 0; 
+
     public static void initializeBoard() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -48,40 +51,46 @@ public class TicTacToe_JavaJawas {
 
 
     public static void main(String[] args) {
+        
         //Declare the variables
         Scanner in = new Scanner(System.in);
         int optiontoChosePLayer = 0;
-        
+
         initializeBoard();
         currentPlayerMark = X;
         gameEnded = false;
 
         //Title to the game
         try {
-            Thread.sleep(150); 
+            Thread.sleep(150);
             println(" _______       ______           ______ ");
-            Thread.sleep(150); 
+            Thread.sleep(150);
             println("/_  __(_)___  /_  __/__ _____  /_  __/__  ___  ");
-            Thread.sleep(150); 
+            Thread.sleep(150);
             println(" / / / / __/   / / / _ `/ __/   / / / _ \\/ -_) ");
-            Thread.sleep(150); 
+            Thread.sleep(150);
             println("/_/ /_/\\__/   /_/  \\_,_/\\__/   /_/  \\___/\\__/  ");
-            Thread.sleep(150); 
+            Thread.sleep(150);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        } 
-                                                         
+        }
+
 
         //Explain the program to the user
         println("\nThis program provide a fun game that is");
         println("created to be enjoyed that is Tic-Tac-Toe,");
         println("where you vs a player or a AI can play");
-        println("and win but also lose or draw."); 
+        println("and win but also lose or draw.");
 
-
+        
 
         while (true) {
+            //Reset core game values 
+            initializeBoard();
+            currentPlayerMark = X;
+            gameEnded = false;
+
             //Menu of selections of gamemodes 
             println("\n-------- Menu -------");
             println("1. Vs a local player");
@@ -92,46 +101,29 @@ public class TicTacToe_JavaJawas {
             println("Please choose an option:");
             optiontoChosePLayer = in.nextInt();
 
-            //Scoreboard
-            int winCount1 = 0;
-            int winCount2 = 0; 
-
-            //leave a space
-            println("\n");
 
             //Vs a local player
-            if (optiontoChosePLayer==1) {
+            if (optiontoChosePLayer == 1) {
 
                 //Option 1: Local player
                 println("\nYou chose to play against a local player");
 
-                //Player 1 name
-                println("\nInput Player 1 name: ");
-                String Player1 = in.next();
+                initializeBoard();
 
-                //Player 2 name
-                println("\nInput Player 2 name: ");
-                String Player2 = in.next();
-
-                
-
-            
-                                                            
                 playGame();
-            
-
-                
-
-                
 
 
-                println("SCOREBOARD"); 
+                println(" "); 
+
+                println("Wins for 'O' player: " + scoreO);
+                println("Wins for 'X' player: " + scoreX);
+                  
 
             }
 
             //Vs an ai machine
-            if (optiontoChosePLayer==2) {
-            
+            if (optiontoChosePLayer == 2) {
+
                 //Option 2: AI player
                 print("\nYou chose to play against the computer");
                 //Player 1 name
@@ -139,246 +131,289 @@ public class TicTacToe_JavaJawas {
                 String Player1 = in.nextLine();
 
 
-                println("SCOREBOARD"); 
+                println("SCOREBOARD");
 
             }
 
-            if (optiontoChosePLayer==3) {
-             
+            if (optiontoChosePLayer == 3) {
                 println("Have a nice day my friend");
-                System.exit(0);
-
+                break;
             }
-                
+         }
+      } 
 
-            //Anything else
-            if (optiontoChosePLayer>3||optiontoChosePLayer<1){
-                
-                println("Error, that is not one of the options please try again");
-            }
+        public static void Scoreboard (char playerMark){
+            //Take in given playerMark at game end
             
+            //Add value to array based on user's character
+            if (playerMark == 'X') {
+                scoreX++;
+            } else {
+                scoreO++; 
             }
-
         }
 
-            //This method will show you the scoreboard of the games won and lose of each player
-            // public static void Scoreboard(int board) {
-            //     int count1 = 0;
-            //     int count2 = 0;
-            //     if (player1Win){
-            //         count1 + 1; 
-            //     }
-            //     if (player2Win){
-            //         count2 + 1;
-            //     }
-            // }
-
-        //This method will track how the match is going
-        public static boolean checkForWin(String player) {
-            return (checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin());
-        }
-
-        //This method is for the AI opponent to go up against
-        public static void AI(int machine) {
-
-        }
-        
-       //Shortcut to use print
-       public static void print(String msg) {
-        System.out.print(msg);
-        }
-
-        //Shortcut to use println
-        public static void println(String msg) {
-        System.out.println(msg);
-        }
-
-        public static boolean checkRowsForWin() {
-            //This method check for the Rows to find out if there is a winner that did a full X or O in the rows that wins the match 
-
-            for (int row =0; row < board.length; row++){
+        public static boolean checkRowsForWin () {
+            //This method check for the Rows to find out if there is a winner that did a full X or O in the rows that wins the match
+            for (int row = 0; row < board.length; row++) {
                 char firstSymbol = board[row][0];
-                boolean rowHasWin = true;
-
-                // Check if all symbols in the row math the first symbol
-                for (int col = 1; col < board[row].length; col++){
-                    if (board[row][col] != firstSymbol){
-                        rowHasWin = false; 
-                        break; 
+                boolean rowHasWin = true; // Assume the row has a win
+        
+                for (int col = 1; col < board[row].length; col++) {
+                    if (board[row][col] != firstSymbol || board[row][col] == EMPTY) {
+                        rowHasWin = false; // If any cell doesn't match or is empty, row doesn't have a win
+                        break;
                     }
                 }
-
-                if (rowHasWin){
-                    return true; 
+        
+                if (rowHasWin) {
+                    return true; // If all cells match, return true
                 }
             }
-
-            return false;
+            return false; // No row has a win
         }
-    
-        public static boolean checkColumnsForWin() {
-            //This method checks for the Columns to find out if there is a winner that did afull X or O in the columns that wins the match
-            int numRows = board.length;
-            int numCols = board[0].length; 
 
-            //Check each column for a win 
-            for (int col = 0; col < numCols; col++) {
-                char firstCell = board[0][col];
-                boolean isWin = true; 
+        public static boolean checkColumnsForWin () {
+            //This method checks for the Columns to find out if there is a winner that did a full X or O in the columns that wins the match
+            for (int col = 0; col < board[0].length; col++) {
+                char firstSymbol = board[0][col];
+                boolean colHasWin = true; // Assume the column has a win
+        
+                for (int row = 1; row < board.length; row++) {
+                    if (board[row][col] != firstSymbol || board[row][col] == EMPTY) {
+                        colHasWin = false; // If any cell doesn't match or is empty, column doesn't have a win
+                        break;
+                    }
+                }
+        
+                if (colHasWin) {
+                    return true; // If all cells match, return true
+                }
+            }
+            return false; // No column has a win
+        }
 
-            //Compare the value of each cell in the column with the first cell 
-            for (int row =1; row < numRows; row++){
-                if (board[row][col] != firstCell){
-                    isWin = false;
+        public static boolean checkDiagonalsForWin () {
+            // This method checks the row to find out if there is a winner that did a full X or O diagonally that wins the match
+            int size = board.length;
+
+            // Check the first diagonal
+            char mainDiagonalCell = board[0][0];
+            boolean mainDiagonalWin = true;
+            for (int i = 1; i < size; i++) {
+                if (board[i][i] != mainDiagonalCell || mainDiagonalCell == ' ') {
+                    mainDiagonalWin = false;
                     break;
                 }
             }
 
-            if (isWin){
-                return true; 
-            }
-          }
-            return false;
-        }
-    
-        public static boolean checkDiagonalsForWin() {
-            //This method checks the row to find out if there is a winner that did a full X or O diagonally that wins the match
-
-            int size = board.length;
-
-            //Check the first diagonal
-            char mainDiagonalCell = board[0][0];
-            boolean mainDiagonalWin = true; 
-            for (int i = 1; i < size; i++){
-                if (board[i][i] != mainDiagonalCell) {
-                    mainDiagonalWin = false; 
-                    break; 
-                }
-            }
-        
-            //Second diagonal 
+            // Second diagonal
             char secondaryDiagonalCell = board[0][size - 1];
-            boolean secondaryDiagonalWin = true; 
-            for (int i = 1; i < size; i++){
-                if (board[i][size - 1 - i] != secondaryDiagonalCell) {
-                    secondaryDiagonalWin = false;
-                    break; 
+            boolean secondaryDiagonalWin = true;
+            if (secondaryDiagonalCell != ' ') { // Check if the diagonal contains non-empty cells
+                for (int i = 1; i < size; i++) {
+                    if (board[i][size - 1 - i] != secondaryDiagonalCell) {
+                        secondaryDiagonalWin = false;
+                        break;
+                    }
                 }
+            } else {
+                secondaryDiagonalWin = false; // Set to false if the diagonal contains empty cells
             }
 
-            //Return true if either diagonal has a win 
-            if (mainDiagonalWin || secondaryDiagonalWin){
-                return true; 
-            }else{
-                return false; //If neither diagonals have a win
-            }
+            // Return true if either diagonal has a win
+            return mainDiagonalWin || secondaryDiagonalWin;
+
         }
 
-        public static boolean boardFull() {
+        public static boolean boardFull () {
             //This method checks for a tie by seeing if all the spots on the board are occupied without a victory
-
-            int numRows = board.length; 
-            int numCols = board[0].length; 
+            int numRows = board.length;
+            int numCols = board[0].length;
 
             //This checks if any cell is empty
             for (int row = 0; row < numRows; row++) {
                 for (int col = 0; col < numCols; col++) {
-                    if (board[row][col] == ' '){
-                        return false; 
+                    if (board[row][col] == ' ') {
+                        return false;
                     }
                 }
             }
 
-            return true; 
+            return true;
         }
 
-       
-    
-        public static void changePlayer() {
-    
-    
+        //This method will track how the match is going
+        public static boolean checkForWin () {
+            if (checkRowsForWin()){
+                gameEnded = true; 
+                return true; 
+            } else if (checkColumnsForWin()){
+                gameEnded = true; 
+                return true; 
+            } else if (checkDiagonalsForWin()){
+                gameEnded = true; 
+                return true;
+            } else if (boardFull()) {
+                gameEnded = true; 
+            }
+            return false; 
         }
 
-
-        public static void printBoard() {
-
-        //   println("\n");
-
-        //   println("----- Here is an empty board :( -----");
-        //   println("");
-        //   println("/    |     |     \\");
-        //   println("|____|_____|_____|");
-        //   println("|    |     |     |");
-        //   println("|____|_____|_____|");
-        //   println("|    |     |     |");
-        //   println("|    |     |     |");
-        //   println("\\----------------/");
+        public static void printBoard () {
+            //This method takes in the initialized array and prints it
 
             for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-              System.out.print(board[i][j]);
-              if(j < SIZE-1) System.out.print("    |"); 
+                for (int j = 0; j < SIZE; j++) {
+                    System.out.print(board[i][j]);
+                    if (j < SIZE - 1) System.out.print("    |");
+                }
+                System.out.println();
+                if (i < SIZE - 1)
+                    println("|____|_____|___|");
             }
-            System.out.println();
-            if(i < SIZE-1)
-              println("|____|_____|___|");
-           }
-          
 
-
-        }
+            }
 
         //This method is used to print out the board
-        public static void playGame() {
-
+        public static void playGame (){
             //Keyboard reader
             Scanner in = new Scanner(System.in);
-    
+
+            //Prompt for player names
+            //Player 1 name
+            println("\nInput Player 1 name: ");
+            String Player1 = in.next();
+
+            //Player 2 name
+            println("\nInput Player 2 name: ");
+            String Player2 = in.next();
+
             //Will run once and loop forever, until user chooses to exit, or some game condition leads to a closing
             do {
-                
-    
                 //Print the current state of the game
+                println(" ");
                 printBoard();
-    
+                println(" ");
+
+                //Confirm player identity 
+                if (currentPlayerMark == X){
+                    println("It's " + Player1 + "'s turn ");
+                }else{
+                    println("It's " + Player2 + "'s turn");
+                }
+
                 //Request input from current player
-                int [] desiredMove = new int[2];
+                while(true) {
+                    int[] desiredMove = new int[2];
+                    
+                    println("Input first your desired x axis position: ");
+                    desiredMove[0] = in.nextInt();
+                    println("Input your desired y axis position");
+                    desiredMove[1] = in.nextInt();
+                    
+                    if(isValidMove(desiredMove[0], desiredMove[1])) {
 
-                println("Input first your desired x axis posistion: ");
-                desiredMove[0] = in.nextInt(); 
-                println("Input your desired y axis posistion");
-                desiredMove[1] = in.nextInt(); 
+                        board[desiredMove[0]][desiredMove[1]] = currentPlayerMark; 
+                        break; 
 
-                //Assign player coordiantes to that's player's mark
-                board[desiredMove[0]][desiredMove[1]] = currentPlayerMark; 
+                    }
+                    
+                    println("Invalid move, try again");
+                  }
 
-                //Check game status (winner? draw?)
 
                 //Check for winner
-                if (checkForWin("X") || checkForWin("O")) {
-                    gameEnded = true; 
-                    break; 
-                }
-
-                //Check for draw
-                if (boardFull()) {
+                if (checkRowsForWin()){
                     gameEnded = true;
-                    break;
+                    printBoard();
+                //Confirm player identity 
+                if (currentPlayerMark == X){
+                    println("Congragulations " + Player1 + "! You won!");
+                }else{
+                    println("Congragulations " + Player2 + "! You won!");
                 }
-    
-            //Modify the 'gameEnded' var here.
-    
-    
-            } while (!gameEnded);
 
-             //Shutdown the Java keyboard reader.
-        in.close();
-    }
+                Scoreboard(currentPlayerMark); 
 
-}
+                } else if (checkColumnsForWin()){
+                    gameEnded = true; 
+                    printBoard();
+                //Confirm player identity 
+                if (currentPlayerMark == X){
+                    println("Congragulations " + Player1 + "! You won!");
+                }else{
+                    println("Congragulations " + Player2 + "! You won!");
+                }
 
+                Scoreboard(currentPlayerMark);
 
+                } else if (checkDiagonalsForWin()){
+                    gameEnded = true; 
+                    printBoard();
+                //Confirm player identity 
+                if (currentPlayerMark == X){
+                    println("Congragulations " + Player1 + "! You won!");
+                }else{
+                    println("Congragulations " + Player2 + "! You won!");
+                }   
+                
+                Scoreboard(currentPlayerMark);
 
+                } else if (boardFull()) {
+                    gameEnded = true; 
+                    printBoard();
+                    println("Game tied.");
+                }
+                
 
+                if (currentPlayerMark == X){
+                    currentPlayerMark = O;
+                }else{
+                    currentPlayerMark = X; 
+                }
+
+                //Modify the 'gameEnded' var here.
+            } while (!gameEnded); 
+        }
+
+           
+
+        
+
+        //This method is for the AI opponent to go up against
+        public static void AI ( int machine){
+
+        }
+        //Shortcut to use print
+        public static void print (String msg){
+            System.out.print(msg);
+        }
+
+        //Shortcut to use println
+        public static void println (String msg){
+            System.out.println(msg);
+        }
+
+        
+        
+        public static boolean isValidMove(int row, int col){
+            //Check if the specified cell is already occupied
+            if (board[row][col] != ' ') {
+                return false; 
+            }else if (row  >= 3){
+                return false; 
+            }else if (col >= 3){
+                return false; 
+            }
+
+            return true;
+        }
+
+        
+
+        
+
+        }
 
 
